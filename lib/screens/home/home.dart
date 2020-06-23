ed18258bfde08bcb9f8c32f4e20a9fed4f235cbd
Kevin H <khoang@ucsb.edu>
2020-06-23 16:32:15 -0700

    Map Screen: Search Bar Added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,3 @@
-import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +10,84 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SearchBar(),
-        ),
+      appBar: AppBar(
+        title: Text("Map Screen"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed:() {
+            showSearch(context: context, delegate: DataSearch());
+          })
+        ],
       ),
-    ); //Scaffold
+      drawer: Drawer(),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String>{
+  final searchResults = [
+    "this",
+    "is",
+    "a",
+    "temporary",
+    "list",
+    "for",
+    "testing"
+  ];
+
+  final recentSearch = [
+    "temporary",
+    "list"
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [IconButton(icon: Icon(Icons.clear), onPressed: () {
+      query = "";
+    })];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+      ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Card(
+      //Put something here if you want something to happen upon clicking result
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentSearch
+        : searchResults.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index)=>ListTile(
+        onTap: (){
+          showResults(context);
+        },
+          title: RichText(
+            text: TextSpan(
+              text: suggestionList[index].substring(0, query.length),
+              style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                  text: suggestionList[index].substring(query.length),
+                  style: TextStyle(color: Colors.grey))
+            ]),
+          ),
+        ),
+      itemCount: suggestionList.length,
+    );
   }
 }
