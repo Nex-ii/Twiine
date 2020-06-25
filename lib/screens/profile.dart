7699507def327f0dd1ad0da3d1fa2855c184c549
jaasonw <jayywong92@gmail.com>
2020-06-24 19:09:49 -0700

    profile page redesign

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twiine/components/Navbar.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -6,116 +7,141 @@ class Profile extends StatefulWidget {
 }
 
 class ProfileState extends State<Profile> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Account Information"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.settings,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              // TODO: add a settings page?
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(height: 20),
-            CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.brown,
-              child: Text('test'),
-            ),
-            Container(height: 30),
-            Text(
-              "FIRSTNAME LASTNAME",
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              ),
-            ),
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                padding: const EdgeInsets.all(20.0),
-                  children: ListTile.divideTiles( //          <-- ListTile.divideTiles
-                  context: context,
-                  tiles: [
-                    ListTile(
-                      title: Text("Profile Details"),
-                      subtitle: Text("FIRSTNAME LASTNAME"),
-                      onTap: () {
-                        // TODO: Edit profile page
-                      },
-                    ),
-                    ListTile(
-                      title: Text("Button 2"),
-                      subtitle: Text("Button 2"),
-                      onTap: () {
-                        // TODO: add button
-                      },
-                    ),
-                    ListTile(
-                      title: Text("Button 3"),
-                      subtitle: Text("Button 3"),
-                      onTap: () {
-                        // TODO: add button
-                      },
-                    ),
-                    ListTile(
-                      title: Text("Button 4"),
-                      subtitle: Text("Button 4"),
-                      onTap: () {
-                        // TODO: add button
-                      },
-                    ),
-                    ListTile(
-                      title: Text("Button 5"),
-                      subtitle: Text("Button 5"),
-                      onTap: () {
-                        // TODO: add button
-                      },
-                    )
-                  ]
-                ).toList()
-              )
+  double cardRadius = 20.0;
+
+  // TODO: we probably don't want to leave this as a url
+  // Returns a card with the name of the place and the image url as the background
+  Container createPlaceCard(String name, String url) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 0, 0, 7),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.9),
+              spreadRadius: -2,
+              blurRadius: 6,
+              offset: Offset(0, 4), // changes position of shadow
             )
           ]
-        )
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels: true,
-        currentIndex: 2,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list, color: Colors.black),
-            title: Text("Feed", style: TextStyle(color: Colors.black))
+        ),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(this.cardRadius),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event, color: Colors.black),
-            title: Text("Scheduled", style: TextStyle(color: Colors.black))
+          semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: Container(
+            width: 220,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(url),
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.white
+                    )
+                  ),
+                ),
+              ],
+            )
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: Colors.black),
-            title: Text("Profile", style: TextStyle(color: Colors.black))
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications, color: Colors.black),
-            title: Text("Requests", style: TextStyle(color: Colors.black))
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today, color: Colors.black),
-            title: Text("Plan", style: TextStyle(color: Colors.black))
-          )
-        ]
+        ),
       )
+    );
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(this.cardRadius),
+                      bottomLeft: Radius.circular(this.cardRadius)
+                    ),
+                    color: Colors.redAccent[400],
+                    boxShadow:  [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.9),
+                          spreadRadius: 3,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                      )
+                    ]
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(height: 40),
+                      CircleAvatar(
+                        backgroundImage: NetworkImage("https://avatars0.githubusercontent.com/u/8981287?s=460&u=4bf37a144d65af7f4d6aa1616fd734f83b566fac&v=4"),
+                        radius: 60,
+                        backgroundColor: Colors.brown,
+                        child: Text('test'),
+                      ),
+                      Container(height: 30),
+                      Text(
+                        "@realwayson",
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.white
+                        )
+                      ),
+                      Container(height: 30),
+                    ],
+                  ),
+                ),
+              ),
+              // Container(height: 20),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+                child: Text(
+                  "Recent Places",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: this.cardRadius),
+                height: 220,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    createPlaceCard("7 Leaves", "https://cdn.vox-cdn.com/thumbor/31J1fxcgvG5a_kT4pMnVhwP9bxM=/0x0:1402x1752/1200x800/filters:focal(522x1001:746x1225)/cdn.vox-cdn.com/uploads/chorus_image/image/64736820/7_leaves_teahouse.0.jpg"),
+                    createPlaceCard("Ding Tea", "https://d1725r39asqzt3.cloudfront.net/aaa7aca1-a5ba-4740-a656-4ef0e5c3e52c/orig.jpg"),
+                    createPlaceCard("7 Leaves", "https://cdn.vox-cdn.com/thumbor/31J1fxcgvG5a_kT4pMnVhwP9bxM=/0x0:1402x1752/1200x800/filters:focal(522x1001:746x1225)/cdn.vox-cdn.com/uploads/chorus_image/image/64736820/7_leaves_teahouse.0.jpg"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ), 
+      ),
+      bottomNavigationBar: Navbar(2),
     );
   }
 }
