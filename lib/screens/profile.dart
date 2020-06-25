a784e818e4d692bf1b99ce0eccedf5d7b951dea9
jaasonw <jayywong92@gmail.com>
2020-06-25 00:58:32 -0700

    cleanup

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -22,7 +22,7 @@ class ProfileState extends State<Profile> {
               color: Colors.grey.withOpacity(0.9),
               spreadRadius: -2,
               blurRadius: 6,
-              offset: Offset(0, 4), // changes position of shadow
+              offset: Offset(0, 4)
             )
           ]
         ),
@@ -38,8 +38,8 @@ class ProfileState extends State<Profile> {
               image: DecorationImage(
                 image: NetworkImage(url),
                 fit: BoxFit.cover,
-                alignment: Alignment.topCenter,
-              ),
+                alignment: Alignment.topCenter
+              )
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -51,16 +51,15 @@ class ProfileState extends State<Profile> {
                   child: Text(
                     name,
                     style: TextStyle(
-                      // fontWeight: FontWeight.bold,
                       fontSize: 30,
                       color: Colors.white
                     )
-                  ),
-                ),
-              ],
+                  )
+                )
+              ]
             )
-          ),
-        ),
+          )
+        )
       )
     );
   }
@@ -79,13 +78,17 @@ class ProfileState extends State<Profile> {
                       bottomRight: Radius.circular(this.cardRadius),
                       bottomLeft: Radius.circular(this.cardRadius)
                     ),
-                    color: Colors.redAccent[200],
-                    boxShadow:  [
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Colors.purpleAccent, Colors.cyan]
+                    ),
+                    boxShadow: [
                         BoxShadow(
                           color: Colors.grey.withOpacity(0.9),
                           spreadRadius: 3,
                           blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
+                          offset: Offset(0, 3)
                       )
                     ]
                   ),
@@ -95,8 +98,7 @@ class ProfileState extends State<Profile> {
                       CircleAvatar(
                         backgroundImage: NetworkImage("https://avatars0.githubusercontent.com/u/8981287?s=460&u=4bf37a144d65af7f4d6aa1616fd734f83b566fac&v=4"),
                         radius: 60,
-                        backgroundColor: Colors.brown,
-                        child: Text('test'),
+                        backgroundColor: Colors.brown
                       ),
                       Container(height: 10),
                       Text(
@@ -109,12 +111,11 @@ class ProfileState extends State<Profile> {
                           color: Colors.white
                         )
                       ),
-                      Container(height: 30),
-                    ],
-                  ),
-                ),
+                      Container(height: 30)
+                    ]
+                  )
+                )
               ),
-              // Container(height: 20),
               Container(
                 padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
                 child: Text(
@@ -122,7 +123,7 @@ class ProfileState extends State<Profile> {
                   style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold
-                  ),
+                  )
                 )
               ),
               Container(
@@ -134,14 +135,14 @@ class ProfileState extends State<Profile> {
                     createPlaceCard("7 Leaves", "https://cdn.vox-cdn.com/thumbor/31J1fxcgvG5a_kT4pMnVhwP9bxM=/0x0:1402x1752/1200x800/filters:focal(522x1001:746x1225)/cdn.vox-cdn.com/uploads/chorus_image/image/64736820/7_leaves_teahouse.0.jpg"),
                     createPlaceCard("Ding Tea", "https://d1725r39asqzt3.cloudfront.net/aaa7aca1-a5ba-4740-a656-4ef0e5c3e52c/orig.jpg"),
                     createPlaceCard("7 Leaves", "https://cdn.vox-cdn.com/thumbor/31J1fxcgvG5a_kT4pMnVhwP9bxM=/0x0:1402x1752/1200x800/filters:focal(522x1001:746x1225)/cdn.vox-cdn.com/uploads/chorus_image/image/64736820/7_leaves_teahouse.0.jpg"),
-                  ],
-                ),
+                  ]
+                )
               )
-            ],
-          ),
-        ), 
+            ]
+          )
+        )
       ),
-      bottomNavigationBar: Navbar(1),
+			bottomNavigationBar: Navbar(1)
     );
   }
 }
