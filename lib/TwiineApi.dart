25aee37746872c771e8738b3c270db2ae854d355
jaasonw <jayywong92@gmail.com>
2020-07-21 19:16:03 -0700

    rename name and parameters for getUser/userExists

--- lib/TwiineApi.dart
+++ lib/TwiineApi.dart
@@ -1,16 +1,14 @@
 import 'dart:collection';
-
 import 'package:cloud_functions/cloud_functions.dart';
 
 class TwiineApi {
   static HttpsCallable _createNewUser = CloudFunctions.instance.getHttpsCallable(functionName: 'createNewUser');
-  // TODO: change this function name in the backend
-  static HttpsCallable _userExists = CloudFunctions.instance.getHttpsCallable(functionName: 'userExists');
+  static HttpsCallable _userExists = CloudFunctions.instance.getHttpsCallable(functionName: 'getUser');
 
-  static Future<LinkedHashMap> getUser(String authType, String data) async {
+  static Future<LinkedHashMap> getUser(String authType, String authField) async {
     return (await TwiineApi._userExists.call(<dynamic, dynamic> {
-      'left_data_field': authType,
-      'right_userdata': data
+      'authType': authType,
+      'authField': authField
     })).data['found'];
   }
 }
