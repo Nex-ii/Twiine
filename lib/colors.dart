b11a56b2633988f9eab33b746d9e83adf750dd2e
jaasonw <jayywong92@gmail.com>
2020-07-25 03:15:38 -0700

    add grey

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/widgets.dart';
 class TwiineColors {
   static final red = Color.fromRGBO(190, 0, 10, 1);
   static final orange = Color.fromRGBO(250, 76, 53, 1);
+  static final grey = Color.fromRGBO(120, 102, 102, 1);
 }
