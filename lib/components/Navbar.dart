f2fa928e06cc8dc011c52676da8ae60d7699b7ae
jaasonw <jayywong92@gmail.com>
2020-07-25 14:54:28 -0700

    indentation

--- lib/components/Navbar.dart
+++ lib/components/Navbar.dart
@@ -33,31 +33,30 @@ class NavbarState extends State<Navbar>{
     return new Scaffold(
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-          onTap: onTappedBar,
-          currentIndex: _currentIndex,
-
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.event, color: Colors.black),
-                title: Text("Scheduled", style: TextStyle(color: Colors.black))
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.black),
-                title: Text("Profile", style: TextStyle(color: Colors.black))
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notifications, color: Colors.black),
-                title: Text("Requests", style: TextStyle(color: Colors.black))
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_today, color: Colors.black),
-                title: Text("Plan", style: TextStyle(color: Colors.black))
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.star, color: Colors.black),
-                title: Text("Favorites", style: TextStyle(color: Colors.black))
-            )
-          ],
+        onTap: onTappedBar,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event, color: Colors.black),
+            title: Text("Scheduled", style: TextStyle(color: Colors.black))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.black),
+            title: Text("Profile", style: TextStyle(color: Colors.black))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications, color: Colors.black),
+            title: Text("Requests", style: TextStyle(color: Colors.black))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, color: Colors.black),
+            title: Text("Plan", style: TextStyle(color: Colors.black))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star, color: Colors.black),
+            title: Text("Favorites", style: TextStyle(color: Colors.black))
+          )
+        ],
       ),
     );
   }
