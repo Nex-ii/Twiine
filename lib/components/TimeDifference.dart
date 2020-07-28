a35d88e3ff8ebc48ea4c546fc470f64528bf4017
jaasonw <jayywong92@gmail.com>
2020-07-27 19:55:32 -0700

    fix time difference briefly showing 0 days

--- lib/components/TimeDifference.dart
+++ lib/components/TimeDifference.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:twiine/colors.dart';
 
 class TimeDifference extends StatefulWidget {
-  final DateTime _eventDate = DateTime(2050, 8, 31);
+  final DateTime _eventDate = DateTime(2020, 10, 31);
 
   @override
   _TimeDifferenceState createState() => _TimeDifferenceState();
 }
 
 class _TimeDifferenceState extends State<TimeDifference> {
-  int difference = DateTime.now().difference(DateTime.now()).inDays;
+  int difference = 0;
   @override
   Widget build(BuildContext context) {
     _updateDate();
+    _updateDateTimer();
     return Container(
       width: 73,
       height: 21,
@@ -32,11 +33,14 @@ class _TimeDifferenceState extends State<TimeDifference> {
       ),
     );
   }
+  void _updateDateTimer() {
+    Future.delayed(Duration(seconds: 10)).then((value) => {
+      _updateDate()
+    });
+  }
   void _updateDate() {
-    Future.delayed(Duration(seconds: 1)).then((value) => {
-      setState(() {
-        difference = widget._eventDate.difference(DateTime.now()).inDays;
-      })
+    setState(() {
+      difference = widget._eventDate.difference(DateTime.now()).inDays;
     });
   }
 }
