037d4ddc724ba5a0dcee727982b2996ff96c0b02
jaasonw <jayywong92@gmail.com>
2020-08-20 21:13:30 -0700

    new red

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 
 class TwiineColors {
-  static final red = Color(0xFFD32F2F);
+  static final red = Color(0xFFF64E5A);
   static final orange = Color(0xFFFF8A80);
   static final grey = Color(0xFF786666);
   static final lightGrey = Color(0xFFE0E0E0);
